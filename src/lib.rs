dd4a2d482c922d936d16aca617d82d530cdcd87c
Moses Palmér <mosespalmer@gmail.com>
2017-01-08 12:37:38 +0100

    Added dependency on ndarray

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
+pub mod room;
+
+extern crate ndarray;
+
 #[cfg(test)]
 mod tests;
---
+++ src/room/mod.rs
@@ -0,0 +1,4 @@
+/// A room is a part of a maze.
+///
+/// It has walls and openings connecting it with other rooms.
+pub trait Room {}

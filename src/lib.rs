7a97313a204ba36cd0de43af7f3fbf2723b0a550
Moses Palmér <mosespalmer@gmail.com>
2017-01-08 12:51:01 +0100

    Added basic maze struct

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,19 @@ pub mod room;
 
 extern crate ndarray;
 
+use ndarray::Array2;
+
+use room::Room;
+
+
+/// A maze is a collection of rooms.
+pub struct Maze<R>
+    where R: Room
+{
+    /// The actual room container
+    rooms: Array2<R>,
+}
+
+
 #[cfg(test)]
 mod tests;

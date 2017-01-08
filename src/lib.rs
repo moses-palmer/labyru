79fdbd07166adb91a0b1830e5e4d2929106c4646
Moses Palmér <mosespalmer@gmail.com>
2017-01-08 19:43:42 +0100

    Added simple constructor for Maze

--- src/lib.rs
+++ src/lib.rs
@@ -16,5 +16,19 @@ pub struct Maze<R>
 }
 
 
+impl<R> Maze<R>
+    where R: Room
+{
+    /// Creates a new maze with all rooms closed.
+    ///
+    /// # Arguments
+    /// * `width` - The width of the maze.
+    /// * `height` - The height of the maze.
+    pub fn new(width: usize, height: usize) -> Maze<R> {
+        Maze { rooms: Array2::from_elem((width, height), R::default()) }
+    }
+}
+
+
 #[cfg(test)]
 mod tests;
--- src/room/mod.rs
+++ src/room/mod.rs
@@ -1,4 +1,4 @@
 /// A room is a part of a maze.
 ///
 /// It has walls and openings connecting it with other rooms.
-pub trait Room {}
+pub trait Room: Clone + Default {}

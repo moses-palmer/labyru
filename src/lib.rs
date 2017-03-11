2b17dd2c2c2e968abeefcbf3006da3325e9f9b60
Moses Palmér <mosespalmer@gmail.com>
2017-03-11 17:23:56 +0100

    Added walls to rooms

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ extern crate ndarray;
 
 pub mod room;
 
+#[macro_use]
+pub mod wall;
+
 #[cfg(test)]
 mod tests;
 
@@ -48,4 +51,89 @@ trait Rooms<T>
 
 
 /// A maze contains rooms and has methods for managing paths and doors.
-trait Maze<T>: Rooms<T> where T: Clone + Default {}
+trait Maze<T>: Rooms<T>
+    where T: Clone + Default
+{
+    /// Returns whether a specified wall is open.
+    ///
+    /// # Arguments
+    /// * `pos` - The room position.
+    /// * `wall` - The wall to check.
+    fn is_open(&self, pos: Pos, wall: &'static wall::Wall) -> bool {
+        match self.get(pos) {
+            Some(room) => room.is_open(wall),
+            None => false,
+        }
+    }
+
+    /// Sets whether a wall is open.
+    ///
+    /// # Arguments
+    /// * `pos` - The room position.
+    /// * `wall` - The wall to modify.
+    /// * `value` - Whether to open the wall.
+    fn set_open(&mut self, pos: Pos, wall: &'static wall::Wall, value: bool) {
+        // First modify the requested wall...
+        if let Some(room) = self.get_mut(pos) {
+            room.set_open(wall, value);
+        }
+
+        // ..and then sync the value on the back
+        let (other_pos, other_wall) = self.back(pos, wall);
+        if let Some(other_room) = self.get_mut(other_pos) {
+            other_room.set_open(other_wall, value);
+        }
+    }
+
+    /// Opens a wall.
+    ///
+    /// # Arguments
+    /// * `pos` - The room position.
+    /// * `wall` - The wall to open.
+    fn open(&mut self, pos: Pos, wall: &'static wall::Wall) {
+        self.set_open(pos, wall, true);
+    }
+
+    /// Closes a wall.
+    ///
+    /// # Arguments
+    /// * `pos` - The room position.
+    /// * `wall` - The wall to close.
+    fn close(&mut self, pos: Pos, wall: &'static wall::Wall) {
+        self.set_open(pos, wall, false);
+    }
+
+    /// Returns the back of a wall.
+    ///
+    /// The back is the other side of the wall, located in a neighbouring room.
+    ///
+    /// # Arguments
+    /// * `pos` - The room position.
+    /// * `wall` - The wall.
+    fn back(&self,
+            pos: Pos,
+            wall: &'static wall::Wall)
+            -> (Pos, &'static wall::Wall) {
+        let other = (pos.0 + wall.dx, pos.1 + wall.dy);
+        (other, self.opposite(other, wall).unwrap())
+    }
+
+    /// Returns the opposite of a wall.
+    ///
+    /// The opposite is the wall located on the opposite side of the room. For
+    /// mazes with rooms with an odd number of walls, there is no opposite wall.
+    ///
+    /// # Arguments
+    /// * `pos` - The room position.
+    /// * `wall` - The wall.
+    fn opposite(&self,
+                pos: Pos,
+                wall: &'static wall::Wall)
+                -> Option<&'static wall::Wall>;
+
+    /// Returns all walls of a specific room.
+    ///
+    /// # Arguments
+    /// * `pos` - The room position.
+    fn walls(&self, pos: Pos) -> &[&'static wall::Wall];
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,7 +1,22 @@
+use std::collections::HashSet;
+
 use ndarray;
 
+use Maze;
+use Pos;
 use Rooms;
 use room;
+use wall;
+
+
+define_walls! {
+    4;
+
+    UP = { dx: 0, dy: -1 },
+    LEFT = { dx: -1, dy: 0},
+    DOWN = { dx: 0, dy: 1},
+    RIGHT = { dx: 1, dy: 0}
+}
 
 
 pub struct TestMaze {
@@ -43,6 +58,22 @@ impl ::Rooms<u32> for TestMaze {
     }
 }
 
+impl ::Maze<u32> for TestMaze {
+    #[allow(unused_variables)]
+    fn opposite(&self,
+                pos: Pos,
+                wall: &'static ::wall::Wall)
+                -> Option<&'static ::wall::Wall> {
+        Some(&walls::ALL[(wall.index + walls::ALL.len() / 2) %
+                         walls::ALL.len()])
+    }
+
+    #[allow(unused_variables)]
+    fn walls(&self, pos: Pos) -> &[&'static wall::Wall] {
+        &walls::ALL
+    }
+}
+
 
 #[test]
 fn width_correct() {
@@ -74,4 +105,41 @@ fn is_inside_correct() {
     assert!(maze.is_inside((width as isize - 1, height as isize - 1)));
     assert!(!maze.is_inside((-1, -1)));
     assert!(!maze.is_inside((width as isize, height as isize)));
+}
+
+
+#[test]
+fn can_open() {
+    let width = 10;
+    let height = 5;
+    let mut maze = TestMaze::new(width, height);
+
+    maze.open((0, 0), &walls::DOWN);
+    assert!(maze.is_open((0, 0), &walls::DOWN));
+    assert!(maze.is_open((0, 1), &walls::UP));
+}
+
+
+#[test]
+fn can_close() {
+    let width = 10;
+    let height = 5;
+    let mut maze = TestMaze::new(width, height);
+
+    maze.open((0, 0), &walls::DOWN);
+    maze.close((0, 1), &walls::UP);
+    assert!(!maze.is_open((0, 0), &walls::DOWN));
+    assert!(!maze.is_open((0, 1), &walls::UP));
+}
+
+
+#[test]
+fn walls_correct() {
+    let width = 10;
+    let height = 5;
+    let maze = TestMaze::new(width, height);
+
+    let walls = maze.walls((0, 1));
+    assert_eq!(walls.iter().cloned().collect::<HashSet<&wall::Wall>>().len(),
+               walls.len());
 }

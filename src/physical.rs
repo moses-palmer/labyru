f28f7829ad1abb6ba2c03e2469676a46f5e0b8f6
Moses Palmér <mosespalmer@gmail.com>
2017-07-27 17:24:59 +0200

    Added method to get positions of wall corners

--- src/physical.rs
+++ src/physical.rs
@@ -1,5 +1,7 @@
 use matrix;
 
+use WallPos;
+
 
 /// A physical position.
 pub type Pos = (f32, f32);
@@ -12,4 +14,23 @@ pub trait Physical {
     /// # Arguments
     /// * `pos` - The matrix position.
     fn center(&self, pos: matrix::Pos) -> Pos;
+
+    /// Returns the "physical" positions of the two corners of a wall.
+    ///
+    /// # Arguments
+    /// * `pos` - The matrix position.
+    /// * `wall` - The wall.
+    fn corners(&self, wall_pos: WallPos) -> (Pos, Pos) {
+        let center = self.center(wall_pos.0);
+        (
+            (
+                center.0 + wall_pos.1.span.0.cos(),
+                center.1 + wall_pos.1.span.0.sin(),
+            ),
+            (
+                center.0 + wall_pos.1.span.1.cos(),
+                center.1 + wall_pos.1.span.1.sin(),
+            ),
+        )
+    }
 }

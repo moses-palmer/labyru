47f1f773096c3414f6a72f50b7b21dd5041c1d3c
Moses Palmér <mosespalmer@gmail.com>
2017-08-29 22:18:07 +0200

    Actually run tests for maze types

--- src/tests.rs
+++ src/tests.rs
@@ -13,7 +13,9 @@ macro_rules! maze_test {
             let width = 10;
             let height = 5;
 
+            $test_function(&mut shape::hex::Maze::new(width, height));
             $test_function(&mut shape::quad::Maze::new(width, height));
+            $test_function(&mut shape::tri::Maze::new(width, height));
         }
     }
 }

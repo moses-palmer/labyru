0b3a732e4d8cd1e489e56d0548bc90f51dcc7e70
Moses Palmér <mosespalmer@gmail.com>
2017-12-18 12:19:33 +0100

    Cleaned up argument parsing

--- src/bin/generate/main.rs
+++ src/bin/generate/main.rs
@@ -12,7 +12,8 @@ extern crate image;
 extern crate rayon;
 
 use std::f32;
-use std::str::FromStr;
+
+use clap::{App, Arg};
 
 use svg::Node;
 
@@ -103,56 +104,63 @@ fn maze_to_viewbox(
 
 #[allow(unused_mut)]
 fn main() {
-    let mut app =
-        clap_app!(myapp =>
-        (about: "Generates mazes")
-        (version: crate_version!())
-        (author: crate_authors!(", "))
-
-        (@arg WALLS:
-            --("walls")
-            +takes_value
-            "The number of walls per room; 3, 4 or 6.")
-
-        (@arg WIDTH:
-            --("width")
-            +takes_value
-            "The width of the maze, in rooms.")
-
-        (@arg HEIGHT:
-            --("height")
-            +takes_value
-            "The height of the maze, in rooms.")
-
-        (@arg SCALE:
-            --("scale")
-            +takes_value
-            "A relative size for the maze, applied to rooms.")
-
-        (@arg MARGIN:
-            --("margin")
-            +takes_value
-            "The margin around the maze.")
-
-        (@arg BREAK:
-            --("break")
-            +takes_value
-            "Whether to break the maze.")
-
-        (@arg HEATMAP:
-            --("heat-map")
-            +takes_value
-            "Whether to create a heat map.")
-
-        (@arg OUTPUT:
-            +required
-            "The output file name.")
-    );
+    let mut app = App::new("")
+        .about("Generates mazes")
+        .version(crate_version!())
+        .author(crate_authors!(", "))
+        .arg(
+            Arg::with_name("WALLS")
+                .long("--walls")
+                .takes_value(true)
+                .default_value("4")
+                .help("The number of walls per room; 3, 4 or 6."),
+        )
+        .arg(
+            Arg::with_name("WIDTH")
+                .long("--width")
+                .takes_value(true)
+                .default_value("12")
+                .help("The width of the maze, in rooms."),
+        )
+        .arg(
+            Arg::with_name("HEIGHT")
+                .long("--height")
+                .takes_value(true)
+                .default_value("9")
+                .help("The height of the maze, in rooms."),
+        )
+        .arg(
+            Arg::with_name("SCALE")
+                .long("--scale")
+                .takes_value(true)
+                .help("A relative size for the maze, applied to rooms."),
+        )
+        .arg(
+            Arg::with_name("MARGIN")
+                .long("--margin")
+                .takes_value(true)
+                .help("The margin around the maze."),
+        )
+        .arg(
+            Arg::with_name("BREAK")
+                .long("--break")
+                .takes_value(true)
+                .help("Whether to break the maze."),
+        )
+        .arg(
+            Arg::with_name("HEATMAP")
+                .long("--heat-map")
+                .takes_value(true)
+                .help("Whether to create a heat map."),
+        )
+        .arg(Arg::with_name("OUTPUT").required(true).help(
+            "The output file name.",
+        ));
 
     #[cfg(feature = "background")]
     {
         app = app.arg(
-            clap::Arg::with_name("BACKGROUND")
+            Arg::with_name("BACKGROUND")
                 .long("background")
                 .help("A background image to colour rooms.")
                 .takes_value(true),
@@ -161,58 +169,40 @@ fn main() {
 
     let args = app.get_matches();
 
-    let maze_type = labyru::MazeType::from_num(
+    let mut maze = labyru::MazeType::from_num(
         args.value_of("WALLS")
-            .map(|s| u32::from_str_radix(s, 10).expect("invalid wall value"))
-            .unwrap_or(4),
-    ).expect("unknown number of walls");
-    let mut maze = maze_type.create(
-        args.value_of("WIDTH")
-            .map(|s| usize::from_str_radix(s, 10).expect("invalid width"))
-            .unwrap_or(12usize),
-        args.value_of("HEIGHT")
-            .map(|s| usize::from_str_radix(s, 10).expect("invalid height"))
-            .unwrap_or(9usize),
-    );
-
-    let scale = args.value_of("SCALE")
-        .map(|s| f32::from_str(s).expect("invalid scale"))
-        .unwrap_or(10.0);
-
-    let margin = args.value_of("MARGIN")
-        .map(|s| f32::from_str(s).expect("invalid margin"))
-        .unwrap_or(10.0);
-
-    let break_action = args.value_of("BREAK").map(|s| {
-        types::break_action::BreakAction::from_str(s)
-            .expect("invalid break")
-    });
-
-    let heat_map_action = args.value_of("HEATMAP").map(|s| {
-        types::heatmap_action::HeatMapAction::from_str(s)
-            .expect("invalid heat map")
-    });
-
-    let background_action = args.value_of("BACKGROUND").map(|s| {
-        types::background_action::BackgroundAction::from_str(s)
-            .expect("invalid background")
-    });
-
-    let mask_action = args.value_of("MASK").map(|s| {
-        types::initialize_action::InitializeAction::from_str(s)
-            .expect("invalid mask")
-    });
-
-    let output = args.value_of("OUTPUT").unwrap();
+            .map(|s| s.parse().expect("invalid wall value"))
+            .unwrap(),
+    ).expect("unknown number of walls")
+        .create(
+            args.value_of("WIDTH")
+                .map(|s| s.parse().expect("invalid width"))
+                .unwrap(),
+            args.value_of("HEIGHT")
+                .map(|s| s.parse().expect("invalid height"))
+                .unwrap(),
+        );
 
     run(
         maze.as_mut(),
-        scale,
-        margin,
-        break_action,
-        heat_map_action,
-        background_action,
-        mask_action,
-        output,
+        args.value_of("SCALE")
+            .map(|s| s.parse().expect("invalid scale"))
+            .unwrap_or(10.0),
+        args.value_of("MARGIN")
+            .map(|s| s.parse().expect("invalid margin"))
+            .unwrap_or(10.0),
+        args.value_of("BREAK").map(
+            |s| s.parse().expect("invalid break"),
+        ),
+        args.value_of("HEATMAP").map(|s| {
+            s.parse().expect("invalid heat map")
+        }),
+        args.value_of("BACKGROUND").map(|s| {
+            s.parse().expect("invalid background")
+        }),
+        args.value_of("MASK").map(
+            |s| s.parse().expect("invalid mask"),
+        ),
+        args.value_of("OUTPUT").unwrap(),
     );
 }

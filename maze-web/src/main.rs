6a71868fa0ce337726ecbfe56c7b27020791963b
Moses Palmér <mosespalmer@gmail.com>
2019-02-08 20:39:15 +0100

    Added entrypoint serving mazes

--- maze-web/src/main.rs
+++ maze-web/src/main.rs
@@ -7,6 +7,74 @@ extern crate svg;
 
 extern crate labyru;
 
+use std::io;
+
+use rocket::http;
+use rocket::response;
+use svg::Node;
+
+use labyru::initialize::randomized_prim::*;
+use labyru::traits::svg::*;
+
+mod types;
+
+/// The maximum nmber of rooms.
+const MAX_ROOMS: usize = 1000;
+
+/// A responder providing an image of a maze.
+struct Maze {
+    maze_type: types::MazeType,
+    dimensions: types::Dimensions,
+    seed: types::Seed,
+}
+
+impl<'a> response::Responder<'a> for Maze {
+    fn respond_to(self, _request: &rocket::Request) -> response::Result<'a> {
+        let room_count = self.dimensions.width * self.dimensions.height;
+        if room_count > MAX_ROOMS {
+            rocket::Response::build()
+                .status(http::Status::InsufficientStorage)
+                .ok()
+        } else {
+            self.into()
+        }
+    }
+}
+
+impl<'a> From<Maze> for response::Result<'a> {
+    fn from(mut source: Maze) -> Self {
+        let mut maze: Box<labyru::Maze> =
+            source.maze_type.create(source.dimensions);
+        maze.randomized_prim(&mut source.seed);
+
+        let mut container = svg::node::element::Group::new();
+        container
+            .append(svg::node::element::Path::new().set("d", maze.to_path_d()));
+        let data = svg::Document::new()
+            .set("viewBox", maze.viewbox())
+            .add(container)
+            .to_string();
+        rocket::Response::build()
+            .sized_body(io::Cursor::new(data))
+            .header(http::ContentType::SVG)
+            .ok()
+    }
+}
+
+#[get("/<maze_type>/<dimensions>/image.svg?<seed>")]
+fn maze_svg<'a>(
+    maze_type: types::MazeType,
+    dimensions: types::Dimensions,
+    seed: types::Seed,
+) -> Maze {
+    Maze {
+        maze_type,
+        dimensions,
+        seed,
+    }
+    .into()
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![]).launch();
+    rocket::ignite().mount("/", routes![maze_svg]).launch();
 }

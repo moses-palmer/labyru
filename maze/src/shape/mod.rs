75dcf3ddec88af0d2ca88a393b50b8a66a38fb46
Moses Palmér <mosespalmer@gmail.com>
2019-05-07 17:28:07 +0200

    Let shapes be serialisable

--- maze/src/shape/mod.rs
+++ maze/src/shape/mod.rs
@@ -63,7 +63,8 @@ macro_rules! define_shape {
 }
 
 /// The different types of mazes implemented, identified by number of walls.
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "lowercase")]
 pub enum Shape {
     /// A maze with triangular rooms.
     Tri = 3,

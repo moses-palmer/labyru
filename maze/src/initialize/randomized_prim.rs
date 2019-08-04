f75eecaa24fdffd6398d433e4c8341d97d250e31
Moses Palmér <mosespalmer@gmail.com>
2019-08-04 12:15:50 +0000

    Clarified Randomized prim documentation

--- maze/src/initialize/randomized_prim.rs
+++ maze/src/initialize/randomized_prim.rs
@@ -3,14 +3,15 @@ use crate::Maze;
 use crate::matrix;
 
 impl Maze {
-    /// Initialises a wall using the _Randomised Prim_ algorithm.
+    /// Initialises a maze using the _Randomised Prim_ algorithm.
     ///
     /// See [here](https://en.wikipedia.org/wiki/Maze_generation_algorithm) for
     /// a description of the algorithm.
     ///
+    /// The maze  should be fully closed; any already open walls will be
+    /// ignored and kept.
+    ///
     /// # Arguments
-    /// *  `maze` - The maze to initialise. This should be a fully closed maze;
-    ///    any already open walls will be ignored and kept.
     /// *  `rng` - A random number generator.
     pub fn randomized_prim<R>(self, rng: &mut R) -> Self
     where
@@ -19,16 +20,17 @@ impl Maze {
         self.randomized_prim_filter(rng, |_| true)
     }
 
-    /// Initialises a wall using the _Randomised Prim_ algorithm.
+    /// Initialises a maze using the _Randomised Prim_ algorithm.
     ///
     /// See [here](https://en.wikipedia.org/wiki/Maze_generation_algorithm) for
     /// a description of the algorithm.
     ///
+    /// The maze  should be fully closed; any already open walls will be
+    /// ignored and kept.
+    ///
     /// This method will ignore rooms for which `filter` returns `false`.
     ///
     /// # Arguments
-    /// *  `maze` - The maze to initialise. This should be a fully closed maze;
-    ///    any already open walls will be ignored and kept.
     /// *  `rng` - A random number generator.
     /// *  `filter` - A predicate filtering rooms to consider.
     pub fn randomized_prim_filter<F, R>(

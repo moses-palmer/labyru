8a531b3cb809d3023a336996285d5dfe4ee03e06
Moses Palmér <mosespalmer@gmail.com>
2019-09-02 16:22:07 +0200

    Let Seed be deserialisable

--- web/src/types/seed.rs
+++ web/src/types/seed.rs
@@ -1,9 +1,12 @@
 use rocket::http;
 use rocket::request;
+use serde::Deserialize;
 
 use maze::initialize;
 
 /// A random seed.
+#[derive(Debug, Deserialize, Eq, PartialEq)]
+#[serde(transparent)]
 pub struct Seed {
     /// The LFSR initialised with the seed.
     lfsr: initialize::LFSR,
@@ -34,5 +37,24 @@ impl initialize::Randomizer for Seed {
 
     fn random(&mut self) -> f64 {
         self.lfsr.random()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_urlencoded;
+
+    use super::*;
+
+    #[test]
+    fn deserialize() {
+        assert_eq!(
+            Seed {
+                lfsr: initialize::LFSR::new(1234)
+            },
+            serde_urlencoded::from_str::<Vec<(String, Seed)>>(&"seed=1234")
+                .unwrap()[0]
+                .1,
+        );
     }
 }

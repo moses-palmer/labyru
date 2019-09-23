435814c1faa0245e5b403cac92b7b1334f79b422
Moses Palmér <mosespalmer@gmail.com>
2019-09-23 20:55:03 +0200

    Added method to calculate distance between physical positions

--- maze/src/physical.rs
+++ maze/src/physical.rs
@@ -10,6 +10,18 @@ pub struct Pos {
     pub y: f32,
 }
 
+impl Pos {
+    /// Returns the distance squared between two points.
+    ///
+    /// # Arguments
+    /// *  `other` - The other point.
+    pub fn distance_squared(&self, other: Self) -> f32 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+        dx * dx + dy * dy
+    }
+}
+
 impl<T> From<(T, T)> for Pos
 where
     T: Into<f32>,
@@ -31,5 +43,17 @@ mod tests {
         let expected = Pos { x: 1.0, y: 2.0 };
         let actual: Pos = (1.0, 2.0).into();
         assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn distance_squared() {
+        assert_eq!(
+            4.0,
+            Pos { x: 0.0, y: 0.0 }.distance_squared((2.0, 0.0).into()),
+        );
+        assert_eq!(
+            4.0,
+            Pos { x: 0.0, y: 0.0 }.distance_squared((0.0, 2.0).into()),
+        );
     }
 }

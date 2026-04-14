e0897868a29ca76b205f8f6fe5feeb8c488620ac
Moses Palmér <mosespalmer@gmail.com>
2026-04-14 19:22:13 +0200

    Let all tools be features

--- tools/src/lib.rs
+++ tools/src/lib.rs
@@ -1,11 +1,15 @@
 #[macro_use]
 extern crate lazy_static;
 
+#[cfg(feature = "alphabet")]
 pub mod alphabet;
+#[cfg(feature = "cell")]
 pub mod cell;
+#[cfg(feature = "heatmap")]
 pub mod heatmap;
+#[cfg(feature = "image")]
 pub mod image;
-pub mod voronoi;
-
 #[cfg(feature = "svg")]
 pub mod svg;
+#[cfg(feature = "voronoi")]
+pub mod voronoi;
